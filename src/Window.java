b6188dfa6149be8d02a3330e6f9c48ef372b64ec
iakon <iakonien@gmail.com>
2022-08-26 23:05:31 +0300

    Можно выбрать разрешение

--- src/Window.java
+++ src/Window.java
@@ -2,13 +2,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
 
 public class Window extends JFrame {
+    String[] resolution = new String[]{  "800x600",
+                                         "1024x768",
+                                         "1200x600",
+                                         "1280x1024",
+                                         "1680x1050",
+                                         "1920x1080"};
 
     public void run(){
+        String dimen = (String) JOptionPane.showInputDialog(Window.this,"Выберите разрешение",
+                "выбор разрешения",JOptionPane.QUESTION_MESSAGE,null,resolution,
+                resolution[0]);
+        int[] kords = Arrays.stream(dimen.split("x")).mapToInt(Integer::valueOf).toArray();
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(1000,800));
+        frame.setPreferredSize(new Dimension(kords[0],kords[1]));
         frame.setLocation(-5,0);
 
         frame.addKeyListener(new KeyAdapter() {
