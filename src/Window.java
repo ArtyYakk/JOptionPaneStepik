d23d4011134d143eca16ec18b4f783c0146d2548
iakon <iakonien@gmail.com>
2022-08-26 22:45:45 +0300

    Небольшой опрос

--- src/Window.java
+++ src/Window.java
@@ -16,10 +16,28 @@ public class Window extends JFrame {
             public void keyPressed(KeyEvent e) {
                 System.out.println(e.getKeyCode());
                 if(e.getKeyCode() == 32){
-                    String name = JOptionPane.showInputDialog(Window.this,"Введите имя",
-                            "Имя",JOptionPane.QUESTION_MESSAGE);
-                    JOptionPane.showMessageDialog(Window.this,("Ваше имя - " +
-                            name), "Имя", JOptionPane.INFORMATION_MESSAGE);
+                    int res1 = JOptionPane.showConfirmDialog(Window.this,"Устраивает" +
+                            " ли вас ваша работа?", "Жоб1",JOptionPane.YES_NO_OPTION);
+                    if(res1 == JOptionPane.YES_OPTION){
+                       JOptionPane.showMessageDialog(Window.this,"Очень рад за вас",
+                               "Жоб1",JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(Window.this,"Удачи с поиском новой",
+                                "Жоб1",JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                    int res2 = JOptionPane.showConfirmDialog(Window.this,"Хотите ли поменять" +
+                            " работу?", "Жоб2",JOptionPane.YES_NO_OPTION);
+                    if(res2 == JOptionPane.YES_OPTION){
+                        JOptionPane.showMessageDialog(Window.this,"Очень ответственный шаг",
+                                "Жоб2",JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(Window.this,"Ну бб тогда",
+                                "Жоб2",JOptionPane.INFORMATION_MESSAGE);
+                    }
+
                 }
             }
         });
